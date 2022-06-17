f690a009f0c85bb4c0d2be064882fa8ddda7cc84
JA Gonzalez <jatgonzalez33@gmail.com>
2022-06-17 15:14:07 -0500

    Orales. Van nuevos cambios

--- saludos jeje/chocolate.js
+++ saludos jeje/chocolate.js
@@ -1,2 +1,22 @@
+function cambiarHeaderDos (nuevoH) {
+    document.getElementsByTagName("h2")[0].innerHTML = nuevoH;
+}
+
+function cambios2000() {
+    let headerDos = document.getElementsByTagName("h2")[0].innerHTML;
+    if (headerDos == "Belinda y Nodal") {
+        cambiarHeaderDos("Aaaaa");
+    }
+    else {
+        cambiarHeaderDos("Belinda y Nodal");
+    }
+};
+
+//Script que se va a unir a la BeliPage
 console.log("Me parece una falta de respeto");
 window.alert("Me parece una falta de respeto!!");
+
+
+//setInterval da un intervalo de tiempo
+//tiene sintaxis: (funcion, cadacCuantosMilisegundos)
+setInterval(cambios2000, 2000)
